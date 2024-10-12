3837b267a2f66a47e018425e1230bce67bbbb71f
AkinniyiS <157753154+AkinniyiS@users.noreply.github.com>
2024-10-12 11:54:34 -0400

    card widget

--- lib/main.dart
+++ lib/main.dart
@@ -51,3 +51,49 @@ class GameScreen extends StatelessWidget {
     );
   }
 }
+
+class CardWidget extends StatelessWidget {
+  final CardModel card;
+  const CardWidget({Key? key, required this.card}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final gameState = context.read<GameState>();
+
+    return GestureDetector(
+      onTap: () {
+        gameState.flipCard(card);
+      },
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        transitionBuilder: (child, animation) => RotationTransition(
+          turns: Tween(begin: 0.5, end: 1.0).animate(animation),
+          child: child,
+        ),
+        child: card.isFaceUp
+            ? Container(
+                color: Colors.blueAccent,
+                child: Center(
+                  child: Text(
+                    card.frontContent,
+                    style: const TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                ),
+              )
+            : Container(
+                color: Colors.grey,
+                child: const Center(
+                  child: Icon(Icons.question_mark, size: 48, color: Colors.white),
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+class CardModel {
+  final String frontContent;
+  bool isFaceUp;
+
+  CardModel({required this.frontContent, this.isFaceUp = false});
+}
